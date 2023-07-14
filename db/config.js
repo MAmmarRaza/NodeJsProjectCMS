6f71dbb9527cf194646ebed2a427c87e3897cf46
MAmmarRaza <20ntucs1120@student.ntu.edu.pk>
2023-07-14 20:25:21 +0500

    commit again with docker

--- db/config.js
+++ db/config.js
@@ -1,3 +1,13 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/blog");
-console.log("connected")
+
+mongoose.connect("mongodb+srv://20ntucs1120:WU6TAoQiyFljPJxO@blog-cluster.bvblv9k.mongodb.net/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
