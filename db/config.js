74da5df807674207d9d7ca8406d1cfa5cb1f76d4
MAmmarRaza <20ntucs1120@student.ntu.edu.pk>
2024-08-31 15:52:22 +0500

    asdf

--- db/config.js
+++ db/config.js
@@ -1,13 +1,24 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+const connectWithRetry = () => {
+  console.log('Attempting to connect to MongoDB...');
 
-mongoose.connect("mongodb+srv://20ntucs1120:WU6TAoQiyFljPJxO@blog-cluster.bvblv9k.mongodb.net/blog", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+    console.log('Retrying in 5 seconds...');
+    setTimeout(connectWithRetry, 5000); // Retry connection after 5 seconds
+  });
+};
+
+connectWithRetry();
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
