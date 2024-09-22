6167ff53e44ea048e2e359c9bfcc93683ccb10d2
MAmmarRaza <20ntucs1120@student.ntu.edu.pk>
2024-09-23 02:05:49 +0500

    ammar

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.use("/admin",restrictAccessMiddleware, userRouter);
 app.use("/", frontRouter);
 
 // Start server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
