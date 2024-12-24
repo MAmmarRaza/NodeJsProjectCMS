b4dd9b0467b336d905a039684deac17dc9b14bd0
MAmmarRaza <20ntucs1120@student.ntu.edu.pk>
2024-12-24 22:41:22 +0500

    asd

--- deploy/application_start.sh
+++ deploy/application_start.sh
@@ -1,3 +1,12 @@
+#create our working directory if it doesnt exist
+DIR="/home/ubuntu/NodeJsProjectCMS"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
+
 cd /home/ubuntu/NodeJsProjectCMS
 pm2 delete index.js
 pm2 start index.js
